8d9d63d6cc3725555f57ea1d5150dbfeb365a2f3
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-10-14 16:09:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here
+let bandNames = ['The Beatles', 'Aerosmith', 'Led Zeppelin', 'Red Hot Chili Peppers', 'The Rolling Stones'];
+
+// Remove articles from band names
+bandNames = bandNames.map(name => name.replace(/^(a|an|the)\s+/i, ''));
+
+// Sort the modified array
+bandNames.sort();
+
+// Get the ul element with id 'band'
+const ul = document.getElementById('band');
+
+// Create and append <li> elements to the ul
+bandNames.forEach(name => {
+  const li = document.createElement('li');
+  li.textContent = name;
+  ul.appendChild(li);
+});
 
