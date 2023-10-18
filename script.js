0bafe9e66253e21476929c1025daefbf56abf258
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-10-18 22:58:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
-// This is a Cypress test
-it('TestingSorting', () => {
-  // Visit your webpage
-  cy.visit('http://localhost:3000'); // Replace with the actual URL of your webpage
-
-  // Verify that there is at least one <li> element
-  cy.get('li').should('have.length.at.least', 1);
-
-  // Get the text content of <li> elements
-  const tags_content = [];
-  cy.get('li').each(($el) => {
-    tags_content.push($el.text());
-  });
-
-  // Check if they are without articles and in sorted order
-  const sorted_content = tags_content
-    .map(tag_content => {
-      // Remove articles from the beginning of each band name
-      return tag_content.replace(/^(A |An |The )/i, '').trim();
-    })
-    .sort();
-
-  // Check if the list is sorted
-  for (let index = 0; index < sorted_content.length; index++) {
-    expect(tags_content[index]).to.eq(sorted_content[index]);
-  }
-});
+ let arr = ['The Virupaksha Temple', 'A Victoria Memorial', 'An Tajmahal'];
+ let withoutArticles =  [] ;
+ let map = {} ;
+ for(let i = 0 ; i < arr.length; i++) {
+    let str = arr[i];
+    str = str.replace(/\ban\b|\bthe\b|\ba\b/gi, "")
+str = str.replace(/ +/gi, " ");
+    str = str.trim();
+    map[str] = arr[i];
+     withoutArticles.push(str);
+ }
+ withoutArticles.sort();
+for(let i = 0; i < withoutArticles.length; i++) {
+     console.log(map[ withoutArticles[i] ]);
+ }
