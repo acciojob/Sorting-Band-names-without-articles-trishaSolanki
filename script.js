3ebdeb1564206e12e70ad30c084afc3a1077e002
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-10-24 15:10:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
  for(let i = 0 ; i < arr.length; i++) {
     let str = arr[i];
     str = str.replace(/\ban\b|\bthe\b|\ba\b/gi, "")
-str = str.replace(/ +/gi, " ");
+   str = str.replace(/ +/gi, " ");
     str = str.trim();
     map[str] = arr[i];
      withoutArticles.push(str);
